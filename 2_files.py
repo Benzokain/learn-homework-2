93a9b83560348cc580f4200588c92dc2a2967dbe
Yury <night1ps@gmail.com>
2022-09-17 02:10:50 +0300

    files_done

--- 2_files.py
+++ 2_files.py
@@ -12,11 +12,18 @@
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    with open('file.txt', 'r', encoding='utf-8') as file:
+        print(f'Длина файла {len(file.read())}')
+
+    with open('file.txt', 'r', encoding='utf-8') as file:
+        count_strings = file.read().split()
+        print(f'Количество слов в файле - {len(count_strings)}')
+
+    with open('file.txt', 'r', encoding='utf-8') as file:
+        read_file = file.read()
+        read_file = read_file.replace('.', '!')
+        with open ('regerat2.txt', 'w', encoding='utf-8') as file_write:
+            file_write.write(read_file)
 
 if __name__ == "__main__":
     main()
